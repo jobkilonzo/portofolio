cc54c8691b2cf6cb0265e31ba79d0649fea7fefd
job kilonzo <jobkilonzo95@gmail.com>
2022-01-14 15:52:50 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Intro from "./component/intro/Intro.jsx";
+const App = () => {
+  return <div><Intro/></div>;
+};
+
+export default App;
---
+++ src/component/intro/Intro.jsx
@@ -0,0 +1,32 @@
+import "./intro.css"
+import Me from "../../images/me.jpg"
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello My name is</h2>
+                    <h1 className="i-name">Job Peter</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Android Development</div>
+                            <div className="i-title-item">UI / UX Design</div>
+                            <div className="i-title-item">Consultant</div>
+                        </div>
+                    </div>
+                    <div className="i-descr">
+                        I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services
+                        and online stores.
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">
+                <div className="i-bg"></div>
+                <img src={Me} alt="" className="i-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Intro
